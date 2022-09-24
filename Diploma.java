d283441a0ecffa45756cc8e9fd298440f9bb3d2e
rochagabs <113868905+rochagabs@users.noreply.github.com>
2022-09-24 19:10:55 -0200

    Add files via upload

---
+++ Diploma.java
@@ -0,0 +1,17 @@
+public class Diploma {
+    private int numero;
+    private String dica;
+
+    public Diploma(int numero, String dica) {
+        this.numero = numero;
+        this.dica = dica;
+    }
+
+    public int diplomaNum() {
+        return numero;
+    }
+    
+    public String getDica() {
+        return dica;
+    }
+}
